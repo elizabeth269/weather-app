061d49bf5fecb833036610d793a4b437e3479ea0
Elizbeth <elizabethadebayo269@gmail.com>
2024-08-20 15:14:50 +0100

    made some changes

--- src/main.rs
+++ src/main.rs
@@ -46,9 +46,9 @@ fn get_weather_info(
     Ok(response_json);
 }
 
-function to display the weather information
+// function to display the weather information
 
-fn display_weather_info(response: &WeatherResponse){
+fn display_weather_info(response: &WeatherResponse) {
     //extract the weather information from the response
     let description: &String = &response.weather[0].description;
 }
